47a88e75d129847f3f74b682dac0eb7b99517d53
YesHouse114 <Yes.house114@gmail.com>
2024-11-11 00:54:20 +0900

    second-launch

--- src/temp_menu/index.js
+++ src/temp_menu/index.js
@@ -1,5 +1,6 @@
-import React from "react"
-import "./styles.css"
+import React from 'react';
+import './styles.css';
+import logo from '../assets/images/00_1JustYe.png';
 
 export default function Menu() {
   return (
@@ -15,7 +16,7 @@ export default function Menu() {
                     alt="Not Found"
                     className="icon-outline-phone"
                   />
-                  <p className="_-4148570107">(414) 857 - 0107</p>
+                  <p className="_-4148570107">(82+)10 - 8267 - 6879</p>
                 </div>
                 <div className="mail">
                   <img
@@ -23,46 +24,28 @@ export default function Menu() {
                     alt="Not Found"
                     className="icon-outline-mail"
                   />
-                  <p className="yummy-bistrobliss">yummy@bistrobliss</p>
+                  <p className="yummy-bistrobliss">yes.house114@gmail.com</p>
                 </div>
               </div>
-              <img
+              {/* <img
                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-10%3A2235?alt=media&token=4fc6fee8-050c-4c0b-a23a-a6858dc9d6d8"
                 alt="Not Found"
                 className="social"
-              />
+              /> */}
             </div>
             <div className="header">
               <div className="logo">
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-10%3A2267?alt=media&token=86820367-1785-494a-b149-6ad2b262849e"
-                  alt="Not Found"
-                  className="japanese-food"
-                />
-                <div className="original-logo">
-                  <p className="cuisine">Bistro Bliss</p>
-                </div>
+                <img src={logo} alt="logo" className="japanese-food" />
               </div>
               <div className="menu-6">
-                <div className="menu">
-                  <p className="home">Home</p>
-                </div>
-                <div className="menu-1">
+                <div className="menu-3">
                   <p className="about">About</p>
                 </div>
-                <div className="menu-3">
-                  <p className="menu-2">Menu</p>
-                </div>
                 <div className="menu-4">
-                  <p className="pages">Pages</p>
+                  <p className="pages">Menu</p>
                 </div>
                 <div className="menu-5">
                   <p className="contact">Contact</p>
-                </div>
-              </div>
-              <div className="button-1">
-                <div className="button">
-                  <p className="get-started-now">Book A Table</p>
                 </div>
               </div>
             </div>
@@ -70,239 +53,31 @@ export default function Menu() {
           <div className="content">
             <div className="title-1">
               <div className="title">
-                <p className="our-menu">Our Menu</p>
+                <p className="our-menu">Ye&apos;s house</p>
                 <p className="we-consider-all-the-driv">
-                  We consider all the drivers of change gives you the components
-                  you need to change to create a truly happens.
+                  is a cafe catering where you can share happiness by sharing
+                  healthy food.
                 </p>
               </div>
-              <div className="button-12">
-                <div className="button-base">
-                  <p className="button-2">All</p>
-                </div>
-                <div className="button-base-1">
-                  <p className="button-4">Breakfast</p>
-                </div>
-                <div className="button-base-2">
-                  <p className="button-6">Main Dishes</p>
-                </div>
-                <div className="button-base-3">
-                  <p className="button-8">Drinks</p>
-                </div>
-                <div className="button-base-4">
-                  <p className="button-10">Desserts</p>
-                </div>
-              </div>
             </div>
-            <div className="cards">
-              <div className="group-750">
-                <div className="_-1">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A716?alt=media&token=800f8bff-8758-4953-83e6-e14afd2a9a8c"
-                    alt="Not Found"
-                    className="mask-group"
-                  />
-                  <div className="text">
-                    <p className="_-999">$ 9.99</p>
-                    <p className="fried-eggs">Fried Eggs</p>
-                    <p className="made-with-eggs-lettuce-s">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-                <div className="_-5">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A752?alt=media&token=0c0c345f-9baf-4af0-a5cc-61420aee10a9"
-                    alt="Not Found"
-                    className="mask-group-1"
-                  />
-                  <div className="text-1">
-                    <p className="_-589">$ 5.89</p>
-                    <p className="mint-lemonade">Mint Lemonade</p>
-                    <p className="made-with-eggs-lettuce-s-1">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="group-478">
-                <div className="_-2">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A725?alt=media&token=813b8b8c-10eb-493f-afeb-05aa08da9f6d"
-                    alt="Not Found"
-                    className="mask-group-2"
-                  />
-                  <div className="text-2">
-                    <p className="_-1599">$ 15.99</p>
-                    <p className="hawaiian-pizza">Hawaiian Pizza</p>
-                    <p className="made-with-eggs-lettuce-s-2">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-                <div className="_-6">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A761?alt=media&token=73ed6e13-0804-478c-a08b-c736a586e994"
-                    alt="Not Found"
-                    className="mask-group-3"
-                  />
-                  <div className="text-3">
-                    <p className="_-1805">$ 18.05</p>
-                    <p className="chocolate-icecream">Chocolate Icecream</p>
-                    <p className="made-with-eggs-lettuce-s-3">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="group-10210">
-                <div className="_-3">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A734?alt=media&token=3d5d4d74-a78c-40f0-a890-26589a6ffc91"
-                    alt="Not Found"
-                    className="mask-group-4"
-                  />
-                  <div className="text-4">
-                    <p className="_-725">$ 7.25</p>
-                    <p className="martinez-cocktail">Martinez Cocktail</p>
-                    <p className="made-with-eggs-lettuce-s-4">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-                <div className="_-7">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A770?alt=media&token=6a40390a-6a28-4b76-9e97-ec39eb4bea80"
-                    alt="Not Found"
-                    className="mask-group-5"
-                  />
-                  <div className="text-5">
-                    <p className="_-1255">$ 12.55</p>
-                    <p className="cheese-burger">Cheese Burger</p>
-                    <p className="made-with-eggs-lettuce-s-5">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="group-853">
-                <div className="_-4">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A743?alt=media&token=a9038508-130c-4814-a8e4-6a91e05c298d"
-                    alt="Not Found"
-                    className="mask-group-6"
-                  />
-                  <div className="text-6">
-                    <p className="_-2099">$ 20.99</p>
-                    <p className="butterscotch-cake">Butterscotch Cake</p>
-                    <p className="made-with-eggs-lettuce-s-6">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-                <div className="_-8">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A779?alt=media&token=dd4eb426-38d7-4905-910c-cb2602b6dba1"
-                    alt="Not Found"
-                    className="mask-group-7"
-                  />
-                  <div className="text-7">
-                    <p className="_-1299">$ 12.99</p>
-                    <p className="classic-waffles">Classic Waffles</p>
-                    <p className="made-with-eggs-lettuce-s-7">
-                      Made with eggs, lettuce, salt, oil and other ingredients.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="brand">
-          <div className="content-1">
-            <div className="title-2">
-              <p className="you-can-order-through-ap">
-                You can order through apps
-              </p>
-              <p className="lorem-ipsum-dolor-sit-am">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit enim
-                bibendum sed et aliquet aliquet risus tempor semper.
-              </p>
-            </div>
-            <img
-              src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-0%3A663?alt=media&token=9c59d5a1-d2ac-4804-a3b2-f0d2ba03f538"
-              alt="Not Found"
-              className="logo-2"
-            />
           </div>
         </div>
         <div className="footer">
           <div className="content-2">
-            <div className="top">
-              <div className="col-1">
-                <div className="logo-4">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-I0%3A656%3B10%3A1347?alt=media&token=685b26e8-3273-4419-a775-0e194e9d0830"
-                    alt="Not Found"
-                    className="japanese-food-1"
-                  />
-                  <div className="original-logo-1">
-                    <p className="cuisine-1">Bistro Bliss</p>
-                  </div>
-                </div>
-                <p className="in-the-new-era-of-technol">
-                  In the new era of technology we look a in the future with
-                  certainty and pride to for our company and.
-                </p>
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-I0%3A656%3B0%3A963?alt=media&token=6c92e6ef-4fc1-44e7-967e-5a26e95a0b99"
-                  alt="Not Found"
-                  className="social-1"
-                />
-              </div>
-              <div className="col-21">
-                <div className="col-2">
-                  <p className="pages-1">Pages</p>
-                  <div className="list">
-                    <p className="home-1">Home</p>
-                    <p className="about-1">About</p>
-                    <p className="menu-8">Menu</p>
-                    <p className="pricing">Pricing</p>
-                    <p className="blog">Blog</p>
-                    <p className="contact-1">Contact</p>
-                    <p className="delivery">Delivery</p>
-                  </div>
-                </div>
-                <div className="col-3">
-                  <p className="utility-pages">Utility Pages</p>
-                  <div className="list-1">
-                    <p className="start-here">Start Here</p>
-                    <p className="styleguide">Styleguide</p>
-                    <p className="password-protected">Password Protected</p>
-                    <p className="_-404-not-found">404 Not Found</p>
-                    <p className="licenses">Licenses</p>
-                    <p className="changelog">Changelog</p>
-                    <p className="view-more">View More</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-31">
-                <p className="follow-us-on-instagram">Follow Us On Instagram</p>
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/4drl7jgycsh-I0%3A656%3B0%3A1009?alt=media&token=2f15e0e4-4194-434b-bd4e-d47fbdafa49b"
-                  alt="Not Found"
-                  className="img"
-                />
-              </div>
-            </div>
             <div className="bottom">
+              <p className="in-the-new-era-of-technol">
+                The official website is being prepared.
+                {/* In the new era of technology we look a in the future with
+                  certainty and pride to for our company and. */}
+              </p>
+
               <p className="copyright-2023-hashtag">
-                Copyright © 2023 Hashtag Developer. All Rights Reserved
+                More updates are coming soon - stay tuned!
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
